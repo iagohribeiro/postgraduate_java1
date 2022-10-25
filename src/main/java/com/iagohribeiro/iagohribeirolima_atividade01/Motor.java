e3859009d17f6ef8ec5524130ee177ffc7ad9d05
IagohRibeiro <iagoh.ribeirolima@gmail.com>
2022-10-25 19:32:22 -0300

    Activity 1 - Implementing a vehicle project

---
+++ src/main/java/com/iagohribeiro/iagohribeirolima_atividade01/Motor.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iagohribeiro.iagohribeirolima_atividade01;
+
+/**
+ *
+ * @author iagohribeiro
+ */
+public class Motor {
+    private int qtdPist;
+    private int potencia;
+    
+    //Default Constructor for the Motor class
+    public Motor() 
+    {
+        qtdPist = 0;
+        potencia = 0;
+    }
+    
+    //Constructor for the Motor class
+    public Motor(int qtdPist, int potencia)
+    {
+        this.qtdPist = qtdPist;
+        this.potencia = potencia;
+    }
+
+    public int getQtdPist() {
+        return qtdPist;
+    }
+
+    public void setQtdPist(int qtdPist) {
+        this.qtdPist = qtdPist;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+}
---
+++ src/main/java/com/iagohribeiro/iagohribeirolima_atividade01/Veiculo.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iagohribeiro.iagohribeirolima_atividade01;
+
+/**
+ *
+ * @author iagohribeiro
+ */
+public class Veiculo {
+    
+    private String placa;       
+    private String marca;
+    private String modelo;
+    private String cor;
+    private float velocMax;
+    private int qtdRodas;
+    private Motor motor;
+
+    public Veiculo() {
+        placa = " ";       
+        marca = " ";
+        modelo = " ";
+        cor = " ";
+        velocMax = 0;
+        qtdRodas = 0;
+        motor = new Motor();
+    }
+
+    public Veiculo(String placa, String marca, String modelo, String cor,
+                    float velocMax, int qtdRodas, int qtdPist, int potencia) {
+        
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.velocMax = velocMax;
+        this.qtdRodas = qtdRodas;
+        this.motor = new Motor (qtdPist, potencia);
+    }
+    
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getVelocMax() {
+        return velocMax;
+    }
+
+    public void setVelocMax(float velocMax) {
+        this.velocMax = velocMax;
+    }
+
+    public int getQtdRodas() {
+        return qtdRodas;
+    }
+
+    public void setQtdRodas(int qtdRodas) {
+        this.qtdRodas = qtdRodas;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+
+    }
+    public void setMotor(int qtdPist, int potencia) {
+        this.motor.setQtdPist(qtdPist);
+        this.motor.setPotencia(potencia);
+    }
+}
+
